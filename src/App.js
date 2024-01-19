66b4cd59b79049148affd753f505030986977772
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-19 07:14:53 +0000

    delete check boostrap buton and create NavBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      
     </div>
   );
 }
