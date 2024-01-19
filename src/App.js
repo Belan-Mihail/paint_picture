d6ccfdd156d5ac43a1bd43bf5d8103cec8143975
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-19 18:28:47 +0000

    create container in App.js for all content and add styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+
+      <Container className={styles.MainContainer}>
+
+      </Container>
     </div>
   );
 }
