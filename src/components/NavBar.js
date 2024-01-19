a8300ba70d6ce65d0e2d025e2c0f609a649ee7e7
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-19 09:24:59 +0000

    connect fontawesome icons to project and add it to nav links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,28 @@
 import React from "react";
-import Container from "react-bootstrap";
-import Navbar from "react-bootstrap";
-import Nav from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 
-function NavBar() {
+const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top">
       <Container>
-        <NavLink to="/">
+        <Nav.Link to="/">
           <Navbar.Brand>
             <span>Paint Pictures</span>
           </Navbar.Brand>
-        </NavLink>
+        </Nav.Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <Nav.Link exact to="/">
-              Home
+            <i className="fas fa-home mr-2"></i>Home
             </Nav.Link>
             <Nav.Link exact to="#">
-              Sign In
+            <i className="fas fa-sign-in-alt mr-2"></i>Sign In
             </Nav.Link>
             <Nav.Link exact to="#">
-              Sign Up
+            <i className="fas fa-user-plus mr-2"></i>Sign Up
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
