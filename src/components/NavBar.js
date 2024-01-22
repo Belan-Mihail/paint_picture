1c794fbb1841e3976bcf191c538575700400dc72
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-22 11:17:26 +0000

    edit navbar component with the addition of useClickOutsideToggle functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,10 +9,13 @@ import { useCurrentUser } from "../context/CurrentUserContext";
 import { useSetCurrentUser } from "../context/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -105,7 +108,12 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar expand="md" fixed="top" className={styles.NavBar}>
+    <Navbar
+      expanded={expanded}
+      expand="md"
+      fixed="top"
+      className={styles.NavBar}
+    >
       <Container>
         <NavLink
           to="/"
@@ -117,7 +125,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPictureOrPlan}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
