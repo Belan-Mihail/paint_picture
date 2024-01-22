f807b79a35a090883c63f7c6a73acd6b1de8ba67
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-22 20:55:45 +0000

    complete Asset component and add styles for it

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import styles from '../styles/Asset.module.css'
+import React from "react";
+import styles from "../styles/Asset.module.css";
+import { Spinner } from "react-bootstrap";
 
-const Asset = () => {
+const Asset = ({ spinner, src, message }) => {
   return (
-    <div>Asset</div>
-  )
-}
+    <div className={`${styles.Asset} p-4`}>
+      {spinner && <Spinner animation="border" className={styles.Spinner} />}
+      {src && <img src={src} alt={message} />}
+      {message && <p className="mt-4">{message}</p>}
+    </div>
+  );
+};
 
-export default Asset
+export default Asset;
