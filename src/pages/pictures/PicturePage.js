c4318306b592788d3eea2ec71a179253a5a777f2
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-23 20:42:46 +0000

    Add initial code in PicturePage component

--- src/pages/pictures/PicturePage.js
+++ src/pages/pictures/PicturePage.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import appStyles from "../../App.module.css";
 
-const PicturePage = () => {
+function PicturePage() {
   return (
-    <div>PicturePage</div>
-  )
-}
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles for mobile</p>
+        <p>Post component</p>
+        <Container className={appStyles.Content}>Comments</Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        Popular profiles for desktop
+      </Col>
+    </Row>
+  );
+};
 
-export default PicturePage
+export default PicturePage;
