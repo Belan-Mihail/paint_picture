aef07b2d84708c1479ffef8e85374c86748bb7ca
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-23 21:20:04 +0000

    create Picture.js and Picture.module.css and add this component to the PicturePage component

--- src/pages/pictures/PicturePage.js
+++ src/pages/pictures/PicturePage.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useParams } from "react-router";
+import Picture from "./Picture";
 
 function PicturePage() {
   const { id } = useParams();
@@ -29,7 +30,7 @@ function PicturePage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
+        <Picture {...picture.results[0]} setPictures={setPicture} PicturePage />
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
