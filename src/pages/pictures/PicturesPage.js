5427feeba780d16a6a44d500132f7fa521710011
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-24 19:25:02 +0000

    add fetchPictures async function to PicturesPage

--- src/pages/pictures/PicturesPage.js
+++ src/pages/pictures/PicturesPage.js
@@ -6,6 +6,21 @@ function PicturesPage({ message, filter = "" }) {
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  useEffect(() => {
+    const fetchPictures = async () => {
+      try {
+        const { data } = await axiosReq.get(`/pictures/?${filter}`);
+        setPictures(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPictures();
+  }, [filter, pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}></Col>
