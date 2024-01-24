9e988d614019071fe616f86c6999e55880e9b3a4
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-24 19:20:46 +0000

    add add main variables with state in PicturesPage component

--- src/pages/pictures/PicturesPage.js
+++ src/pages/pictures/PicturesPage.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "../../styles/PicturesPage.module.css";
 
-function PicturesPage() {
+function PicturesPage({ message, filter = "" }) {
+  const [pictures, setPictures] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
   return (
     <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-
-      </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        
-      </Col>
+      <Col className="py-2 p-0 p-lg-2" lg={8}></Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2"></Col>
     </Row>
   );
 }
