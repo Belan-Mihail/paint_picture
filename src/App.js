2a7c0318072afb358232123bbfa63338a3e92a04
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-24 18:59:34 +0000

    Add route for PicturesPage as main page. add route for feed and like links in navbar

--- src/App.js
+++ src/App.js
@@ -8,15 +8,41 @@ import SignUpForm from "./pages/auth/SignUpForm.js";
 import SignInForm from "./pages/auth/SignInForm.js";
 import PictureCreateForm from "./pages/pictures/PictureCreateForm.js";
 import PicturePage from "./pages/pictures/PicturePage.js";
+import PicturesPage from "./pages/pictures/PicturesPage.js";
+import { useCurrentUser } from "./context/CurrentUserContext";
 
 function App() {
+
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
 
       <Container className={styles.MainContainer}>
       <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/" render={() => <PicturesPage />} />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PicturesPage 
+                message="No results found. Adjust the search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/liked"
+            render={() => (
+              <PicturesPage 
+                message="No results found. Adjust the search keyword or like a post."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/pictures/create" render={() => <PictureCreateForm />} />
