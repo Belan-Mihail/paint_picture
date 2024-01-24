8df9790071dd5c1fb912f81c49f03c69af20dfc3
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-24 18:47:03 +0000

    create PicturesPage components and PicturesPage.module.css

---
+++ src/pages/pictures/PicturesPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../../styles/PicturesPage.module.css'
+
+function PicturesPage() {
+  return (
+    <div>PicturesPage</div>
+  )
+}
+
+export default PicturesPage
