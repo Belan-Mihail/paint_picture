2efce662a673831a62aba278a9db581ffe31506b
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-24 19:04:04 +0000

    Add preparatory markup code for PicturesPage component

--- src/pages/pictures/PicturesPage.js
+++ src/pages/pictures/PicturesPage.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import styles from '../../styles/PicturesPage.module.css'
+import React from "react";
+import styles from "../../styles/PicturesPage.module.css";
 
 function PicturesPage() {
   return (
-    <div>PicturesPage</div>
-  )
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        
+      </Col>
+    </Row>
+  );
 }
 
-export default PicturesPage
+export default PicturesPage;
