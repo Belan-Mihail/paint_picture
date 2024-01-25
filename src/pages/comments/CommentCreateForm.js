bb20640e6ce5050989eeebe76aee9c5c45f73837
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-25 20:48:47 +0000

    create CommentCreateForm.js and CommentCreateForm.module.css

---
+++ src/pages/comments/CommentCreateForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../../styles/CommentCreateForm.module.css'
+
+function CommentCreateForm() {
+  return (
+    <div>CommentCreateForm</div>
+  )
+}
+
+export default CommentCreateForm
