4cfb658aa2ad06c50297f3d7b57adeee57e2e0fb
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-25 16:55:29 +0000

    Add additional changes to MoreDropdown component and add css styles to it

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -15,18 +15,27 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
 
 export const MoreDropdown = () => {
   return (
-    <Dropdown>
-      <Dropdown.Toggle as={ThreeDots} id="dropdown-custom-components">
-        Custom toggle
-      </Dropdown.Toggle>
+    <Dropdown className="ml-auto" drop="left">
+      <Dropdown.Toggle as={ThreeDots} />
 
-      <Dropdown.Menu popperConfig={{ strategy: "fixed" }}>
-        <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-        <Dropdown.Item eventKey="3" active>
-          Orange
+      <Dropdown.Menu
+        className="text-center"
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={() => {}}
+          aria-label="edit"
+        >
+          <i className="fas fa-edit" />
         </Dropdown.Item>
-        <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={() => {}}
+          aria-label="delete"
+        >
+          <i className="fas fa-trash-alt" />
+        </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
