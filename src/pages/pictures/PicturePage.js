ea2b8d430c58fbc913fdb1087a63038f1ddd883b
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-25 21:00:50 +0000

    Edit PicturePage component for displaying comments

--- src/pages/pictures/PicturePage.js
+++ src/pages/pictures/PicturePage.js
@@ -6,20 +6,25 @@ import appStyles from "../../App.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useParams } from "react-router";
 import Picture from "./Picture";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../context/CurrentUserContext";
 
 function PicturePage() {
   const { id } = useParams();
   const [picture, setPicture] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
         const [{ data: picture }] = await Promise.all([
           axiosReq.get(`/pictures/${id}`),
-          
         ]);
         setPicture({ results: [picture] });
-        console.log(picture)
+        console.log(picture);
       } catch (err) {
         console.log(err);
       }
@@ -33,7 +38,19 @@ function PicturePage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Picture {...picture.results[0]} setPictures={setPicture} PicturePage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              picture={id}
+              setPicture={setPicture}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
