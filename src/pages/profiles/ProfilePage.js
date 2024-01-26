589b9ebef07ed7214113a4512c207cb729a14893
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-26 19:17:50 +0000

    Add necessary consts and edit useEffect to fetch profile data

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -14,10 +14,39 @@ import btnStyles from "../../styles/Button.module.css";
 
 import PopularProfiles from "./PopularProfiles";
 import { useCurrentUser } from "../../context/CurrentUserContext";
+import { useParams } from "react-router";
+import {
+    useProfileData,
+    useSetProfileData,
+  } from "../../context/ProfileDataContext";
+  import { axiosReq } from "../../api/axiosDefaults";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
+  const { id } = useParams();
+  const setProfileData = useSetProfileData();
+  const { pageProfile } = useProfileData();
+  const [profile] = pageProfile.results;
+  const is_owner = currentUser?.username === profile?.owner;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [{ data: pageProfile }] = await Promise.all([
+          axiosReq.get(`/profiles/${id}/`),
+        ]);
+        setProfileData((prevState) => ({
+          ...prevState,
+          pageProfile: { results: [pageProfile] },
+        }));
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [id, setProfileData]);
 
   const mainProfile = (
     <>
@@ -62,12 +91,7 @@ function ProfilePage() {
 
   return (
     <Row>
-      <Col
-        className="py-2 p-0 p-lg-2"
-        lg={8}
-        data-aos="fade-right"
-        data-aos-duration="1000"
-      >
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
         <PopularProfiles mobile />
         <Container className={appStyles.Content}>
           {hasLoaded ? (
