5a5f681028d8554dfb53590bd94cfe7cae2342cd
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-26 16:05:09 +0000

    Add necessary consts with state to ProfileDataProvider

--- src/context/ProfileDataContext.js
+++ src/context/ProfileDataContext.js
@@ -1,5 +1,22 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "../context/CurrentUserContext";
 
-export const ProfileDataProvider = () => {
+const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+    const [profileData, setProfileData] = useState({
+
+        pageProfile: { results: [] },
+        popularProfiles: { results: [] },
+      });
+    
+      const currentUser = useCurrentUser();
+
   return (
     <div></div>
   )
