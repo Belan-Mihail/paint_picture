443ea7127668359c3c92f3fae54cc5a75f568457
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-26 12:11:11 +0000

    add to CommentEditForm component JSX

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -1,1 +1,37 @@
+
+
+import React from "react";
 import styles from '../../styles/CommentCreateEditForm.module.css'
+import Form from "react-bootstrap/Form";
+import btnStyles from "../../styles/Button.module.css";
+
+function CommentEditForm(props) {
+    return (
+        <Form>
+          <Form.Group className="pr-1">
+            <Form.Control
+              className={styles.Form}
+              as="textarea"
+              rows={2}
+            />
+          </Form.Group>
+          <div className="text-right">
+            <button
+              className={`${btnStyles.Button} ${btnStyles.CommentButton}`}
+              type="button"
+            >
+              cancel
+            </button>
+            <button
+              className={`${btnStyles.Button} ${btnStyles.SaveButton} mr-1 ml-1`}
+              
+              type="submit"
+            >
+              save
+            </button>
+          </div>
+        </Form>
+      );
+}
+
+export default CommentEditForm
