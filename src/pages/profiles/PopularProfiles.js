6c8371d505b1d255953dbb604828fa35ce146c65
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-26 13:56:33 +0000

    Add necessary consta and handleMount function to Popular profiles component

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,13 +1,44 @@
-import appStyles from '../../App.module.css'
-import React from 'react'
+import appStyles from "../../App.module.css";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
+import Asset from "../../components/Asset";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+import { axiosReq } from "../../api/axiosDefaults";
 
 const PopularProfiles = () => {
+  const [profileData, setProfileData] = useState({
+    popularProfiles: { results: [] },
+  });
+
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
   return (
     <Container className={`${appStyles.Content} mt-4`}>
-        <p>Popular profiles</p>
+      <p>Popular profiles</p>
+      {popularProfiles.results.map((profile) => (
+        <p key={profile.id}>{profile.owner}</p>
+      ))}
     </Container>
-  )
-}
+  );
+};
 
-export default PopularProfiles
+export default PopularProfiles;
