907af718487b0fd0f832a6055f4fd5cbb9420006
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-26 12:24:47 +0000

    Add handleChange function and necessary consts to CommentEditForm component

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -1,37 +1,45 @@
-
-
 import React from "react";
-import styles from '../../styles/CommentCreateEditForm.module.css'
+import styles from "../../styles/CommentCreateEditForm.module.css";
 import Form from "react-bootstrap/Form";
 import btnStyles from "../../styles/Button.module.css";
 
 function CommentEditForm(props) {
-    return (
-        <Form>
-          <Form.Group className="pr-1">
-            <Form.Control
-              className={styles.Form}
-              as="textarea"
-              rows={2}
-            />
-          </Form.Group>
-          <div className="text-right">
-            <button
-              className={`${btnStyles.Button} ${btnStyles.CommentButton}`}
-              type="button"
-            >
-              cancel
-            </button>
-            <button
-              className={`${btnStyles.Button} ${btnStyles.SaveButton} mr-1 ml-1`}
-              
-              type="submit"
-            >
-              save
-            </button>
-          </div>
-        </Form>
-      );
+  const { id, content, setShowEditForm, setComments } = props;
+
+  const [formContent, setFormContent] = useState(content);
+
+  const handleChange = (event) => {
+    setFormContent(event.target.value);
+  };
+
+  return (
+    <Form>
+      <Form.Group className="pr-1">
+        <Form.Control
+          className={styles.Form}
+          as="textarea"
+          rows={2}
+          value={formContent}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <div className="text-right">
+        <button
+          className={`${btnStyles.Button} ${btnStyles.CommentButton}`}
+          onClick={() => setShowEditForm(false)}
+          type="button"
+        >
+          cancel
+        </button>
+        <button
+          className={`${btnStyles.Button} ${btnStyles.SaveButton} mr-1 ml-1`}
+          type="submit"
+        >
+          save
+        </button>
+      </div>
+    </Form>
+  );
 }
 
-export default CommentEditForm
+export default CommentEditForm;
