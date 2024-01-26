b4bcdf1f6a2c5eb071734ee34e0d055ce70151ce
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-26 14:30:40 +0000

    add mobile display PopularProfiles component

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -5,7 +5,7 @@ import Asset from "../../components/Asset";
 import { useCurrentUser } from "../../context/CurrentUserContext";
 import { axiosReq } from "../../api/axiosDefaults";
 
-const PopularProfiles = () => {
+const PopularProfiles = ({ mobile }) => {
   const [profileData, setProfileData] = useState({
     popularProfiles: { results: [] },
   });
@@ -32,13 +32,23 @@ const PopularProfiles = () => {
   }, [currentUser]);
 
   return (
-    <Container className={`${appStyles.Content} mt-4`}>
+    <Container className={`${appStyles.Content} ${
+      mobile && "d-lg-none text-center mb-3"
+    } mt-4`}>
       {popularProfiles.results.length ? (
         <>
           <p>Most followed profiles.</p>
-          {popularProfiles.results.map((profile) => (
-            <p key={profile.id}>{profile.owner}</p>
-          ))}
+          {mobile ? (
+            <div className="d-flex justify-content-around">
+              {popularProfiles.results.slice(0, 4).map((profile) => (
+                <p key={profile.id}>{profile.owner}</p>
+              ))}
+            </div>
+          ) : (
+            popularProfiles.results.map((profile) => (
+              <p key={profile.id}>{profile.owner}</p>
+            ))
+          )}
         </>
       ) : (
         <Asset spinner />
