4f4c6eb6c4d5286b01e016e2a18c2ba9e52db43f
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-28 21:45:10 +0000

    Add base JSX to Plan component

--- src/pages/plans/Plan.js
+++ src/pages/plans/Plan.js
@@ -1,26 +1,43 @@
-import React from 'react'
+import React from "react";
 import { useCurrentUser } from "../../context/CurrentUserContext";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
 
 const Plan = (props) => {
-    const {
-        id,
-        owner,
-        profile_id,
-        profile_image,
-        plans_title,
-        plans_description,
-        plans_date,
-        until,
-        updated_at,
-        setProfilePlans,
-      } = props;
-    
-      const currentUser = useCurrentUser();
-      const is_owner = currentUser?.username === owner;
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    plans_title,
+    plans_description,
+    plans_date,
+    until,
+    updated_at,
+    setProfilePlans,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
-    <div>Plan</div>
-  )
-}
+    <>
+      <Row>
+        <Col className="text-center mt-2">
+          {plans_title && <p className="text-center">{plans_title}</p>}
+        </Col>
+      </Row>
+      <Row className="mb-2">
+        {plans_description && <Col xs={8} className="text-center">{plans_description}</Col>}
+        {until && <Col className="text-center">until</Col>}
+        {plans_date && <Col className="text-center">{plans_date}</Col>}
+      </Row>
+    </>
+  );
+};
 
-export default Plan
+export default Plan;
