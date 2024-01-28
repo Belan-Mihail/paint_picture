164278272ce4ae983a7411b5e826d45190748a03
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-28 19:07:35 +0000

    Add to UsernameForm component necessary imports

--- src/pages/profiles/UsernameForm.js
+++ src/pages/profiles/UsernameForm.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
+
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
 const UsernameForm = () => {
     return (
         <Row>
