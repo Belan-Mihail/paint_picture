f2b08d410ec52aa463d3b1a90756105a28b82f44
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-29 10:33:48 +0000

    Add necessary consts and useState to ProfileEditForm and add render JSX

--- src/pages/profiles/ProfileEditForm.js
+++ src/pages/profiles/ProfileEditForm.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useHistory, useParams } from "react-router-dom";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -10,7 +11,29 @@ import Container from "react-bootstrap/Container";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
+import { axiosReq } from "../../api/axiosDefaults";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../context/CurrentUserContext";
+
 const ProfileEditForm = () => {
+    const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+  const { id } = useParams();
+  const history = useHistory();
+  const imageFile = useRef();
+
+  const [profileData, setProfileData] = useState({
+    name: "",
+    content: "",
+    image: "",
+    greeting: "",
+  });
+  const { name, content, image, greeting } = profileData;
+
+  const [errors, setErrors] = useState({});
+
   const textFields = (
     <>
       <Form.Group>
@@ -34,7 +57,49 @@ const ProfileEditForm = () => {
     </>
   );
 
-  return <div>ProfileEditForm</div>;
+  return (
+    <Form onSubmit={() => {}}>
+      <Row>
+        <Col className="py-2 p-0 p-md-2 text-center" md={7} lg={6}>
+          <Container className={appStyles.Content}>
+            <Form.Group>
+              {image && (
+                <figure>
+                  <Image src={image} fluid />
+                </figure>
+              )}
+
+              <div>
+                <Form.Label
+                  className={`${btnStyles.Button} ${btnStyles.Main} btn my-auto`}
+                  htmlFor="image-upload"
+                >
+                  Change the image
+                </Form.Label>
+              </div>
+              <Form.File
+                id="image-upload"
+                ref={imageFile}
+                accept="image/*"
+                onChange={(e) => {
+                  if (e.target.files.length) {
+                    setProfileData({
+                      ...profileData,
+                      image: URL.createObjectURL(e.target.files[0]),
+                    });
+                  }
+                }}
+              />
+            </Form.Group>
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={6} className="d-none d-md-block p-0 p-md-2 text-center">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
 };
 
 export default ProfileEditForm;
