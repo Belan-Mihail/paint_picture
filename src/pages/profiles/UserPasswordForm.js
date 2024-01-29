4e471686cb347c66e6f97ccdf96dc4ae9a23f83f
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-29 09:42:46 +0000

    Add handleChange function to UserPasswordComponent

--- src/pages/profiles/UserPasswordForm.js
+++ src/pages/profiles/UserPasswordForm.js
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import React from "react";
 
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
@@ -10,46 +9,80 @@ import Form from "react-bootstrap/Form";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
+import { useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+
 const UserPasswordForm = () => {
-    return (
-        <Row>
-          <Col className="py-2 mx-auto text-center" md={6}>
-            <Container className={appStyles.Content}>
-              <Form onSubmit={() => {}}>
-                <Form.Group>
-                  <Form.Label>New password</Form.Label>
-                  <Form.Control
-                    placeholder="new password"
-                    type="password"
-                    name="new_password1"
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Confirm password</Form.Label>
-                  <Form.Control
-                    placeholder="confirm new password"
-                    type="password"
-                    name="new_password2"
-                  />
-                </Form.Group>
+  const history = useHistory();
+  const { id } = useParams();
+  const currentUser = useCurrentUser();
 
-                <Button
-                  className={`${btnStyles.Button} ${btnStyles.Main}`}
-                  onClick={() => {}}
-                >
-                  cancel
-                </Button>
-                <Button
-                  type="submit"
-                  className={`${btnStyles.Button} ${btnStyles.Main}`}
-                >
-                  save
-                </Button>
-              </Form>
-            </Container>
-          </Col>
-        </Row>
-      );
-}
+  const [userData, setUserData] = useState({
+    new_password1: "",
+    new_password2: "",
+  });
+  const { new_password1, new_password2 } = userData;
 
-export default UserPasswordForm
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (currentUser?.profile_id?.toString() !== id) {
+      history.push("/");
+    }
+  }, [currentUser, history, id]);
+
+  return (
+    <Row>
+      <Col className="py-2 mx-auto text-center" md={6}>
+        <Container className={appStyles.Content}>
+          <Form onSubmit={() => {}}>
+            <Form.Group>
+              <Form.Label>New password</Form.Label>
+              <Form.Control
+                placeholder="new password"
+                type="password"
+                name="new_password1"
+                value={new_password1}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Confirm password</Form.Label>
+              <Form.Control
+                placeholder="confirm new password"
+                type="password"
+                name="new_password2"
+                value={new_password2}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Main}`}
+              onClick={() => {}}
+            >
+              cancel
+            </Button>
+            <Button
+              type="submit"
+              className={`${btnStyles.Button} ${btnStyles.Main}`}
+            >
+              save
+            </Button>
+          </Form>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default UserPasswordForm;
