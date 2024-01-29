c8161b53e829e3acff4f2aaab224bd71b2b33d77
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-29 07:19:22 +0000

    Add MoreDropdown component to Plan component

--- src/pages/plans/Plan.js
+++ src/pages/plans/Plan.js
@@ -4,6 +4,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import styles from "../../styles/Plan.module.css";
 import { Container } from "react-bootstrap";
+import { MoreDropdown } from "../../components/MoreDropdown";
 
 const Plan = (props) => {
   const {
@@ -20,35 +21,33 @@ const Plan = (props) => {
   const is_owner = currentUser?.username === owner;
 
   return (
-    
     <Container>
       <Row>
-        
         <Col className="text-center mt-2">
           {plans_title && (
             <p className={`${styles.PlansTitle} text-center`}>{plans_title}</p>
           )}
         </Col>
-        
+
+        {is_owner && (
+          <MoreDropdown handleEdit={() => {}} handleDelete={() => {}} />
+        )}
       </Row>
       <Row className={`${styles.DescRow} d-flex mb-2`}>
-        
-          {plans_description && (
-            <Col className={`${styles.PlansCol} text-center`}>
-              {plans_description}
-            </Col>
-          )}
-          {until && (
-            <Col className={`${styles.PlansCol} ${styles.Until} text-center`}>
-              until
-            </Col>
-          )}
-          {plans_date && <Col className="text-center">{plans_date}</Col>}
-        
+        {plans_description && (
+          <Col className={`${styles.PlansCol} text-center`}>
+            {plans_description}
+          </Col>
+        )}
+        {until && (
+          <Col className={`${styles.PlansCol} ${styles.Until} text-center`}>
+            until
+          </Col>
+        )}
+        {plans_date && <Col className="text-center">{plans_date}</Col>}
       </Row>
       <hr className={styles.PlansDivider}></hr>
-      </Container>
-    
+    </Container>
   );
 };
 
