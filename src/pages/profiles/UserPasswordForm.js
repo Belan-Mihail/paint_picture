7d3e7f2b6fb216cf390b0f11acfbc56d2a69f975
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-29 09:49:27 +0000

    Add handleSubmit functiona and Alerts to UserPasswordForm component

--- src/pages/profiles/UserPasswordForm.js
+++ src/pages/profiles/UserPasswordForm.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
 
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
@@ -40,11 +41,22 @@ const UserPasswordForm = () => {
     }
   }, [currentUser, history, id]);
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.post("/dj-rest-auth/password/change/", userData);
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+      setErrors(err.response?.data);
+    }
+  };
+
   return (
     <Row>
       <Col className="py-2 mx-auto text-center" md={6}>
         <Container className={appStyles.Content}>
-          <Form onSubmit={() => {}}>
+          <Form onSubmit={handleSubmit}>
             <Form.Group>
               <Form.Label>New password</Form.Label>
               <Form.Control
@@ -55,6 +67,11 @@ const UserPasswordForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            {errors?.new_password1?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
             <Form.Group>
               <Form.Label>Confirm password</Form.Label>
               <Form.Control
@@ -65,7 +82,11 @@ const UserPasswordForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
-
+            {errors?.new_password2?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
             <Button
               className={`${btnStyles.Button} ${btnStyles.Main}`}
               onClick={() => {}}
