41819f941cfe5c5ee175bddd8b5a9db16e430115
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-30 20:18:33 +0000

    Add JSX code to Wallitem component

--- src/pages/wallitems/Wallitem.js
+++ src/pages/wallitems/Wallitem.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "../../styles/Comment.module.css";
+import Media from "react-bootstrap/Media";
+import Link from "react-router-dom/Link";
+import Avatar from "../../components/Avatar";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+
 
 const Wallitem = (props) => {
   const {
+    profile_id,
     profile_image,
     owner,
     updated_at,
@@ -10,8 +17,23 @@ const Wallitem = (props) => {
     setProfileWallItems,
   } = props;
 
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
   return (
-    <div>Wallitem</div>
+    <div>
+      <hr />
+      <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} height={40} />
+        </Link>
+        <Media.Body className="align-self-center ml-2">
+          <span className={styles.Owner}>{owner}</span>
+          <span className={styles.Date}>{updated_at}</span>
+            <p>{message}</p>
+        </Media.Body>
+      </Media>
+    </div>
   )
 }
 
