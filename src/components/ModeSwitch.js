3be8b51d43c482ce61f56895e468a05bb910df70
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-31 17:03:37 +0000

    create ModeSwitch.js

---
+++ src/components/ModeSwitch.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState, useEffect } from "react";
+import { ThemesModeContext } from "./../context/ThemesModeContext";
+import styles from '../styles/Select.module.css'
+
+function ModeSwitch() {
+
+
+const { setThemesMode } =
+    useContext(ThemesModeContext);
+
+
+  const options = [
+    {value: '', text: '--choose theme--'},
+    {value: 'light', text: 'Light'},
+    {value: 'turquoise', text: 'Turquoise'},
+    {value: 'orange', text: 'Orange'},
+  ];
+
+  const [selected, setSelected] = useState(options[0].value);
+
+  const handleChange = (event) => {
+    setSelected(event.target.value);
+
+  };
+
+
+  useEffect(() => {
+    setThemesMode(selected)
+    }, [setThemesMode, selected]);
+
+  
+
+  return (
+    <div className="mt-2">
+      <select className={styles.Select} value={selected} onChange={handleChange}>
+        {options.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.text}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default ModeSwitch;
