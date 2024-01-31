9ba31bdd38f478e2b81b50270e4abf9900b7f9fa
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-01-31 15:39:41 +0000

    Create PictureOrderingFilter.js

---
+++ src/components/PictureOrderingFilter.js
@@ -0,0 +1,49 @@
+import React, { useContext, useState } from "react";
+import { PictureOrderingFilterContext } from "./../context/PictureOrderingFiltersContext";
+
+
+function PictureOrderingFilter() {
+
+
+const { setPictureOrderingFilter } =
+    useContext(PictureOrderingFilterContext);
+
+
+  const options = [
+    {value: '', text: '--filtred by default--'},
+    {value: 'likes_count', text: 'Likes (Low to High)'},
+    {value: '-likes_count', text: 'Likes (High to Low)'},
+    {value: 'comments_count', text: 'Comments (Low to High)'},
+    {value: '-comments_count', text: 'Comments (High to Low)'},
+  ];
+
+  const [selected, setSelected] = useState(options[0].value);
+
+  const handleChange = (event) => {
+    setSelected(event.target.value);
+
+  };
+
+
+
+  useEffect(() => {
+    setPictureOrderingFilter(selected)
+    }, [setPictureOrderingFilter, selected]);
+
+  
+
+  return (
+    <div>
+
+      <select value={selected} onChange={handleChange}>
+        {options.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.text}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default PictureOrderingFilter;
