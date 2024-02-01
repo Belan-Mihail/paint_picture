f68de3d2e4e4556a70f091bc2d50891e680d48ce
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-01 11:57:01 +0000

    Add additional test for asset component

--- src/components/__test__/Assets.test.js
+++ src/components/__test__/Assets.test.js
@@ -2,28 +2,40 @@ import { render, screen } from "@testing-library/react";
 import Asset from "../Asset";
 import { BrowserRouter as Router } from "react-router-dom";
 
-describe('Asset renders', () => {
-
-    test('renders the Asset component', () => {
-      const renderComponent = () => render(<Asset />);
-      const renderAsset = renderComponent();
-      expect(renderAsset).not.toBeNull();
-    });
-
-    test("renders Asset component prop message if define ", () => {
-        const renderComponent = () =>
-          render(
-            <Router>
-              <Asset message="test" />
-            </Router>
-          );
-    
-        const renderAsset = renderComponent();
-        const screenHasMessage = screen.getByText("test");
-    
-        expect(renderAsset.getByText("test")).toBeInTheDocument();
-        expect(screenHasMessage).toBeInTheDocument();
-      });
-
-
+describe("Asset renders", () => {
+  test("renders the Asset component", () => {
+    const renderComponent = () => render(<Asset />);
+    const renderAsset = renderComponent();
+    expect(renderAsset).not.toBeNull();
   });
+
+  test("renders Asset component prop message if define ", () => {
+    const renderComponent = () =>
+      render(
+        <Router>
+          <Asset message="test" />
+        </Router>
+      );
+
+    const renderAsset = renderComponent();
+    const screenHasMessage = screen.getByText("test");
+
+    expect(renderAsset.getByText("test")).toBeInTheDocument();
+    expect(screenHasMessage).toBeInTheDocument();
+  });
+
+  test("renders Asset component prop src if it is define and alt if message prop is define ", () => {
+    const renderComponent = () =>
+      render(
+        <Router>
+          <Asset src="image.jpeg" message="test" />
+        </Router>
+      );
+
+    const renderAsset = renderComponent();
+    const assetImageAlt = screen.getByAltText("test");
+
+    expect(renderAsset.getByRole("img")).toBeInTheDocument();
+    expect(assetImageAlt).toBeInTheDocument();
+  });
+});
