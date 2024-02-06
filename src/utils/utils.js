a49162ab0bf721b2c302e672888506e70d18937c
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-06 19:57:36 +0000

    Add shouldRefreshToken and removeTokenTimestamp to untils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -51,3 +51,11 @@ export const setTokenTimestamp = (data) => {
   const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
   localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
 };
+
+export const shouldRefreshToken = () => {
+  return !!localStorage.getItem("refreshTokenTimestamp");
+};
+
+export const removeTokenTimestamp = () => {
+  localStorage.removeItem("refreshTokenTimestamp");
+};
